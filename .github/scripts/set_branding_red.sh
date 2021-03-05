7161fbc84ce96e0d4919c021dcf84b024c98d3c8
Alexander Schulze <alexander.schulze@gmail.com>
2021-03-05 15:13:27 +0100

    Update app target in Podfile

--- .github/scripts/set_branding_red.sh
+++ .github/scripts/set_branding_red.sh
@@ -2,8 +2,13 @@
 
 set -euxo pipefail
 
+# Set vue environment
 mv .env .env.debug
 mv .env.red .env
 
+# ios / set icons
 rm -r ios/App/App/Assets.xcassets/AppIcon.appiconset
 mv brands/red/ios/AppIcon.appiconset ios/App/App/Assets.xcassets/AppIcon.appiconset
+
+# ios / set app target
+sed -i '' 's/AppOrange/AppRed/g' ./ios/App/Podfile
