3f731b39202991c793904a7b31bb02819f5a36ec
Alexander Schulze <alexander.schulze@gmail.com>
2021-03-06 23:56:32 +0100

    Updated ios build files

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
 xcodebuild -archivePath $PWD/ios/build/App.xcarchive \
-            -target App$TARGET
+            -target App$TARGET \
             -exportOptionsPlist ios/App/App/exportOptions.plist \
             -exportPath $PWD/ios/build \
             -exportArchive | xcpretty
