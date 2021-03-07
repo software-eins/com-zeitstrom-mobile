e04305286c7e6b9ce4ef02a4ed9013674ae63626
Alexander Schulze <alexander.schulze@gmail.com>
2021-03-07 20:42:46 +0100

    Added export options

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -5,7 +5,6 @@ set -eo pipefail
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
 xcodebuild -archivePath $PWD/ios/build/App.xcarchive \
-            -target App$TARGET \
-            -exportOptionsPlist ios/App/App/exportOptions.plist \
+            -exportOptionsPlist ios/App/exportOptions$TARGET.plist \
             -exportPath $PWD/ios/build \
             -exportArchive | xcpretty
