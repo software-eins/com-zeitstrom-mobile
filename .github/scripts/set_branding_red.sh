07ea75b02a2ec31cac9511e9910abbd2a1bf98ac
Alexander Schulze <alexander.schulze@gmail.com>
2021-03-07 22:01:07 +0100

    Store ios icons for different brands within project

--- .github/scripts/set_branding_red.sh
+++ .github/scripts/set_branding_red.sh
@@ -5,7 +5,3 @@ set -euxo pipefail
 # Set vue environment
 mv .env .env.debug
 mv .env.red .env
-
-# ios / set icons
-rm -r ios/App/App/Assets.xcassets/AppIcon.appiconset
-mv brands/red/ios/AppIcon.appiconset ios/App/App/Assets.xcassets/AppIcon.appiconset
