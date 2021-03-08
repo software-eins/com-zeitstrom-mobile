d547c14abeb587c36430a3d48506043bc8476949
Alexander Schulze <alexander.schulze@gmail.com>
2021-03-08 09:47:12 +0100

    Refactor workflow structure

--- .github/scripts/set_branding_orange.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-mv .env .env.debug
-mv .env.orange .env
--- .github/scripts/set_branding_red.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-# Set vue environment
-mv .env .env.debug
-mv .env.red .env
