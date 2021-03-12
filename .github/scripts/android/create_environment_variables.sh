8d8c0fdf33b0bdcdb35100c47968efae9f0ebd79
Alexander Schulze <alexander.schulze@gmail.com>
2021-03-12 12:52:25 +0100

    Added initial android deployment flow

---
+++ .github/scripts/android/create_environment_variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+rm -f envars
+
+echo "export APP_BRAND=$BRAND" >> envars
+echo "export APP_LABEL=$LABEL" >> envars
+echo "export APP_PACKAGE=$PACKAGE" >> envars
+
+echo "export APP_VERSION_CODE=`git rev-list --all --count`" >> envars
+echo "export APP_VERSION_MARKETING=3.1.`date +"%y%m%d"`" >> envars
+
+echo "export VUE_APP_BRANDING=$BRAND" >> envars
