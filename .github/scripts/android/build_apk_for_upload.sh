14d10f54d436a12a1764f3a0b7106cc9a771c770
Alexander Schulze <alexander.schulze@gmail.com>
2021-03-12 14:14:04 +0100

    Updated android build flow

---
+++ .github/scripts/android/build_apk_for_upload.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e pipefail
+
+cd android
+
+rm -f ./app/build/outputs/apk/release/app-release-unsigned.apk
+rm -f ./app/build/outputs/apk/release/app.apk
+
+./gradlew build
+
+jarsigner -sigalg SHA1withRSA \
+    -digestalg SHA1 \
+    -storepass kmuZFTJYGyNKw9YmdCW822 \
+    -keystore ../.github/secrets/android/2018-08-20-upload-keystore.jks \
+    ./app/build/outputs/apk/release/app-release-unsigned.apk time_tracking
+
+zipalign -v 4 \
+    ./app/build/outputs/apk/release/app-release-unsigned.apk \
+    ./app/build/outputs/apk/release/app.apk
--- .github/scripts/android/sign_apk_for_upload.sh
+++
@@ -1,4 +0,0 @@
-
-jarsigner -sigalg SHA1withRSA -digestalg SHA1 -storepass kmuZFTJYGyNKw9YmdCW822 \
-    -keystore /Users/alex/projects/de.triumph-adler.mobile/signing-keys/android.jks \
-    /Users/alex/projects/com.zeitstrom/web3/zeitblick/android/app/build/outputs/apk/release/app-release-unsigned.apk time_tracking
