3636f663c8f68981eafdb58020b2c10e73ea978c
Alexander Schulze <alexander.schulze@gmail.com>
2021-03-15 05:16:01 +0100

    Updated android CD

--- .github/scripts/android/decrypt_secrets.sh
+++ .github/scripts/android/decrypt_secrets.sh
@@ -7,4 +7,4 @@ gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" \
 
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" \
     --output ./.github/secrets/android/2021-03-13-service-account.json \
-    ./.github/secrets/android/2021-03-13-service-account.json
+    ./.github/secrets/android/2021-03-13-service-account.json.gpg
