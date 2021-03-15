df1e9392fcccbb9a319e5522f5a62443a87c6c76
Alexander Schulze <alexander.schulze@gmail.com>
2021-03-15 06:48:31 +0100

    Debug Android CD

--- .github/scripts/android/create_environment_variables.sh
+++ .github/scripts/android/create_environment_variables.sh
@@ -8,7 +8,7 @@ echo "export APP_BRAND=$BRAND" >> envars
 echo "export APP_LABEL=$LABEL" >> envars
 echo "export APP_PACKAGE=$PACKAGE" >> envars
 
-echo "export APP_VERSION_CODE=`git rev-list --all --count`" >> envars
+echo "export APP_VERSION_CODE=`expr $GITHUB_RUN_NUMBER + 90`" >> envars
 echo "export APP_VERSION_MARKETING=3.1.`date +"%y%m%d"`" >> envars
 
 echo "export VUE_APP_BRANDING=$BRAND" >> envars
