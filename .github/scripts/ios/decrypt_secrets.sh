9d189001f2787cc39c096018f7941abed25074c7
Alexander Schulze <alexander.schulze@gmail.com>
2021-04-07 10:02:42 +0200

    Added ios mobileprovision for black

--- .github/scripts/ios/decrypt_secrets.sh
+++ .github/scripts/ios/decrypt_secrets.sh
@@ -6,16 +6,19 @@ SECRETS_PATH=./.github/secrets/ios
 KEYPAIR=2021-02-25-Keypair.p12
 PROFILE_ORANGE=2021-02-25-orange.mobileprovision
 PROFILE_RED=2021-03-04-red.mobileprovision
+PROFILE_BLACK=2021-04-07-black.mobileprovision
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_KEY" --output $SECRETS_PATH/$KEYPAIR $SECRETS_PATH/$KEYPAIR.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_KEY" --output $SECRETS_PATH/$PROFILE_ORANGE $SECRETS_PATH/$PROFILE_ORANGE.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_KEY" --output $SECRETS_PATH/$PROFILE_RED $SECRETS_PATH/$PROFILE_RED.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_KEY" --output $SECRETS_PATH/$PROFILE_BLACK $SECRETS_PATH/$PROFILE_BLACK.gpg
 
 
 # Add mobile provisioning profiles
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp $SECRETS_PATH/$PROFILE_ORANGE ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_ORANGE
 cp $SECRETS_PATH/$PROFILE_RED ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_RED
+cp $SECRETS_PATH/$PROFILE_BLACK ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_BLACK
 
 # Add signing certificate to keychain
 security create-keychain -p "" build.keychain
