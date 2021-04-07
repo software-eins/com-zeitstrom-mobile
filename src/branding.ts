6a03c3426788b772cb3f41661149f229b098078c
Alexander Schulze <alexander.schulze@gmail.com>
2021-04-07 11:29:43 +0200

    Added black brand to UI

--- src/branding.ts
+++ src/branding.ts
@@ -18,6 +18,22 @@ brands.set("orange", {
     supportPhoneFootnote: 'Mo-Do 7 – 17 Uhr, Fr 7 - 16 Uhr, 20 Cent aus dem deutschen Festnetz, Mobilfunkpreise können abweichen, maximal jedoch 60 Cent pro Anruf',
 });
 
+brands.set("black", {
+    id: "black",
+    name: "UTAX Zeitblick",
+    url: "utax-zeitblick.de",
+    colors: {
+        primary: '#FF8200',
+        success: '#28C76F',
+        danger: '#EA5455',
+        warning: '#FF9F43',
+        dark: '#1E1E1E',
+    },
+    isMobile: process.env.VUE_APP_IS_MOBILE == 'true',
+    supportPhone: '+49 1806 456326',
+    supportPhoneFootnote: 'Mo-Do 7 – 17 Uhr, Fr 7 - 16 Uhr, 20 Cent aus dem deutschen Festnetz, Mobilfunkpreise können abweichen, maximal jedoch 60 Cent pro Anruf',
+});
+
 brands.set("red", {
     id: "red",
     name: "Zeitstrom",
