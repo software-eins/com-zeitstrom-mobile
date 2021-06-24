fed23683c50b118e01752c4decf685790a0e40e6
Alexander Schulze <alexander.schulze@gmail.com>
2021-06-24 17:21:05 +0200

    Updated signing and zipaligning

--- .github/scripts/android/build_apk_for_upload.sh
+++ .github/scripts/android/build_apk_for_upload.sh
@@ -8,13 +8,13 @@ rm -f ./app/build/outputs/apk/release/app.apk
 
 ./gradlew build --no-daemon
 
-/usr/local/lib/android/sdk/build-tools/30.0.3/zipalign -v 4 \
 # ~/Library/Android/sdk/build-tools/30.0.2/zipalign -v 4 \
+/usr/local/lib/android/sdk/build-tools/30.0.3/zipalign -v 4 \
     ./app/build/outputs/apk/release/app-release-unsigned.apk \
     ./app/build/outputs/apk/release/app.apk
 
-/usr/local/lib/android/sdk/build-tools/30.0.3/apksigner sign \
 # ~/Library/Android/sdk/build-tools/30.0.2/apksigner sign \
+/usr/local/lib/android/sdk/build-tools/30.0.3/apksigner sign \
     --ks-pass pass:kmuZFTJYGyNKw9YmdCW822 \
     --ks ../.github/secrets/android/2018-08-20-upload-keystore.jks \
     --ks-key-alias \
