63aaa3bb09c3a6ca48d2fb0d2c2d6d675897d39a
Alexander Schulze <alexanderschulze@users.noreply.github.com>
2021-06-28 12:09:35 +0200

    Update create_environment_variables.sh

--- .github/scripts/android/create_environment_variables.sh
+++ .github/scripts/android/create_environment_variables.sh
@@ -8,7 +8,7 @@ echo "export APP_BRAND=$BRAND" >> envars
 echo "export APP_LABEL=$LABEL" >> envars
 echo "export APP_PACKAGE=$PACKAGE" >> envars
 
-echo "export APP_VERSION_CODE=`expr $GITHUB_RUN_NUMBER + 90`" >> envars
+echo "export APP_VERSION_CODE=`expr $GITHUB_RUN_NUMBER + 100`" >> envars
 echo "export APP_VERSION_MARKETING=3.1.`date +"%y%m%d%H%M"`" >> envars
 
 echo "export VUE_APP_BRANDING=$BRAND" >> envars
