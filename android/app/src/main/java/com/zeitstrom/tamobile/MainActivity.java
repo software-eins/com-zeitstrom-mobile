c19951c6443be2eb88f4a297eb5aa12709207617
Alexander Schulze <alexander.schulze@gmail.com>
2021-06-28 09:04:00 +0200

    Updated android project

--- android/app/src/main/java/com/zeitstrom/tamobile/MainActivity.java
+++ android/app/src/main/java/com/zeitstrom/tamobile/MainActivity.java
@@ -1,15 +1,5 @@
 package com.zeitstrom.tamobile;
 
-import android.app.ActionBar;
-import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.zeitstrom.zs_mobile.R;
-
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {}
