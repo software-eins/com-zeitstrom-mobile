2bf663663fd7f867de0538b3ab372946aa763669
Alexander Schulze <alexander.schulze@gmail.com>
2021-07-05 10:02:55 +0200

    Set env vars for android development

--- develop.sh
+++ develop.sh
@@ -1,1 +1,13 @@
+export APP_LABEL="TA Zeitblick"
+export APP_BRAND="orange"
+export VUE_APP_BRANDING=$APP_BRAND
+export APP_PACKAGE="com.zeitstrom.ta_mobile"
+export APP_VERSION_CODE=142
+export APP_VERSION_MARKETING=3.1.211201
+export BRAND=$APP_BRAND
+export LABEL=$APP_LABEL
+export PACKAGE=$APP_PACKAGE
+export BASE_URL="http://192.168.2.230:8000"
+export VUE_APP_BASE_URL=$BASE_URL
+
 ionic capacitor run ios -l --external
